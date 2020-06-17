12af9919871a4149068d4b283ef4866db96e19b3
lchow <Drqufc913>
2020-06-17 11:50:41 +0200

    Add AppComponent and BaseApplication features

---
+++ app/src/main/java/com/lchowaniec/daggerexample/di/AppComponent.kt
@@ -0,0 +1,23 @@
+package com.lchowaniec.daggerexample.di
+
+import android.app.Application
+import com.lchowaniec.daggerexample.BaseApplication
+import dagger.BindsInstance
+import dagger.Component
+import dagger.android.AndroidInjectionModule
+import dagger.android.AndroidInjector
+import dagger.android.support.AndroidSupportInjectionModule
+
+@Component(modules = [AndroidSupportInjectionModule::class])
+interface AppComponent:AndroidInjector<BaseApplication> {
+
+    @Component.Builder
+    interface Builder{
+
+        @BindsInstance
+        fun application(application: Application): Builder
+
+        fun build(): AppComponent
+    }
+
+}
