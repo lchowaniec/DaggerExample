95db944f4f7687d51ae34e147d7c5f684ca9b95e
lchow <Drqufc913>
2020-06-17 15:27:35 +0200

    Add @Singleton annotation to AppModule functions and AppCompononet

--- app/src/main/java/com/lchowaniec/daggerexample/di/AppModule.kt
+++ app/src/main/java/com/lchowaniec/daggerexample/di/AppModule.kt
@@ -9,23 +9,25 @@ import com.bumptech.glide.request.RequestOptions
 import com.lchowaniec.daggerexample.R
 import dagger.Module
 import dagger.Provides
+import javax.inject.Singleton
 
 @Module
 class AppModule {
     companion object{
+        @Singleton
         @Provides
         fun provideRequestOptions(): RequestOptions{
             return RequestOptions
                 .placeholderOf(R.drawable.white_background)
                 .error(R.drawable.white_background)
         }
-
+        @Singleton
         @Provides
         fun provideGlideInstance(application: Application,requestOptions: RequestOptions):RequestManager{
             return Glide.with(application)
                 .setDefaultRequestOptions(requestOptions)
         }
-
+        @Singleton
         @Provides
         fun provideAppDrawable(application: Application): Drawable? {
             return ContextCompat.getDrawable(application,R.drawable.logo)
